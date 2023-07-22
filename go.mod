84f7d984e62d8a3cce9232c65b2ac2eb593edfe4
David McIntosh <804610+mctofu@users.noreply.github.com>
2023-07-22 00:47:48 +0000

    setup inner source dependency

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ghes-version-update-test-with-private-go-module-on-ghes-instance
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"${{ghes_host}}/${{ghes_org}}/ghes-version-update-test-with-private-go-module-on-ghes-instance/hello"
+)
+
+func main() {
+	fmt.Printf("Hello %s\n", hello.Who())
+}
